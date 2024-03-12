7225f27aea39cc44cecf0c09000983f7ce36f5c8
cberches <cberches@acquiro.com.ph>
2024-03-12 11:43:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -376,7 +376,7 @@ with tab3:
         else:
             prompt = str(jd) + " " + str(app_info)
 
-        print(prompt)
+        st.write(prompt)
         ai_score_placeholder = st.empty()
         full_response = ""
         for response in openai.ChatCompletion.create(
