db56334a41a17bf96e4a11e3ea2f1e1bfc61dcb0
cberches <cberches@acquiro.com.ph>
2024-03-14 08:59:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ with tab1:
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
-    prompt = st.empty()
-    if prompt.chat_input("What is up?"):
+    
+    if prompt := st.chat_input("What is up?"):
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
             st.markdown(prompt)
