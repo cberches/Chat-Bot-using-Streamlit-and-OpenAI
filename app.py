6da57b4d77f13f6766411c2033eb63cb85116392
cberches <cberches@acquiro.com.ph>
2024-04-30 14:34:49 +0800

    Added a chatbot version 2

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import openai
 import streamlit as st
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, ServiceContext, Document
+from llama_index.llms.openai import OpenAI
 
-tab1, tab2, tab3, tab4 = st.tabs(["Chatbot", "JD Generator", "Job Matching", "AI Screening"])
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["Chatbotv1", "Chatbotv2","JD Generator", "Job Matching", "AI Screening"])
 
+openai.api_key = st.secrets['OPENAI_API_KEY']
 
 with tab1:
     st.write("Currently Not Working")
 
     st.title("NXT Chatbot")
-    openai.api_key = st.secrets['OPENAI_API_KEY']
+    
 
     if "openai_model" not in st.session_state:
         st.session_state["openai_model"] = "gpt-3.5-turbo"
@@ -245,7 +248,7 @@ with tab1:
             message_placeholder.markdown(full_response)
         st.session_state.messages.append({"role": "assistant", "content": full_response})
 
-with tab2:
+with tab3:
     prompt = st.text_input("Describe the position/job role")
     system_prompt = """
     Context:
@@ -288,7 +291,7 @@ with tab2:
             message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
 
-with tab3:
+with tab4:
     
     system_prompt = """# CONTEXT # 
     I am a machine learning engineer at a startup company in the staffing solutions industry. In this field, I create machine learning models which will help the company to achieve their goals. Now, I am focused on talent recruitment by helping them to develop AI models specifically for the Talent Recruitment platform as well as the application tracking system.
@@ -394,7 +397,7 @@ with tab3:
             ai_score_placeholder.markdown(full_response)
         ai_score_placeholder.markdown(full_response)
 
-with tab4:
+with tab5:
     system_prompt = """# CONTEXT # 
         I am a machine learning engineer at a startup company in the staffing solutions industry. In this field, I create machine learning models which will help the company to achieve their goals. Now, I am focused on talent recruitment by helping them to develop AI models specifically for the Talent Recruitment platform as well as the application tracking system.
 
@@ -495,3 +498,39 @@ with tab4:
             full_response += response.choices[0].delta.get("content", "")
             ai_screening_placeholder.markdown(full_response + "▌")
         ai_screening_placeholder.markdown(full_response)
+
+with tab2:
+    st.header("Chat with the Streamlit docs 💬 📚")
+
+    if "messages" not in st.session_state.keys(): # Initialize the chat message history
+        st.session_state.messages = [
+            {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
+        ]
+    
+
+    @st.cache_resource(show_spinner=False)
+    def load_data():
+        with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
+            reader = SimpleDirectoryReader(input_dir="./kb", recursive=True)
+            docs = reader.load_data()
+            service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
+            index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+            return index
+
+    index = load_data()
+
+    chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+    if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+    for message in st.session_state.messages: # Display the prior chat messages
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+    
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                response = chat_engine.chat(prompt)
+                st.write(response.response)
+                message = {"role": "assistant", "content": response.response}
+                st.session_state.messages.append(message) # Add response to message history
