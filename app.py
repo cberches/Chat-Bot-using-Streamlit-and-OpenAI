a207b0793268d638c035d2eee65523a2e81ee2b5
cberches <cberches@acquiro.com.ph>
2024-05-08 09:52:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ with tab1:
 
         chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
         if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
-            st.session_state.messages.append({"role": "user", "content": prompt +" Do not answer if you know it is wrong."})
+            st.session_state.messages.append({"role": "user", "content": prompt})
 
         for message in st.session_state.messages: # Display the prior chat messages
             with st.chat_message(message["role"]):
@@ -52,7 +52,7 @@ with tab1:
         if st.session_state.messages[-1]["role"] != "assistant":
             with st.chat_message("assistant"):
                 with st.spinner("Thinking..."):
-                    response = chat_engine.chat(prompt)
+                    response = chat_engine.chat(prompt+"Do not answer question not related to topic.")
                     st.write(response.response)
                     message = {"role": "assistant", "content": response.response }
                 st.session_state.messages.append(message) # Add response to message history
