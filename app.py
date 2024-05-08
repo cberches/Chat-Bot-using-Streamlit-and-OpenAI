390613d02ee607726dfb694073ba304f2141de42
cberches <cberches@acquiro.com.ph>
2024-05-08 09:23:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -20,46 +20,43 @@ with tab1:
     st.title("NXT Chatbot")
     isV2 = st.toggle("Use v2 with index")
     if isV2: 
-        try:
-            st.header("Chatbot v2")
-            st.subheader("Using Llama Indexing for document file-based knowledge base")
+        st.header("Chatbot v2")
+        st.subheader("Using Llama Indexing for document file-based knowledge base")
 
-            if "messages" not in st.session_state.keys(): # Initialize the chat message history
-                st.session_state.messages = [
-                    {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
-                ]
-            
+        if "messages" not in st.session_state.keys(): # Initialize the chat message history
+            st.session_state.messages = [
+                {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
+            ]
+        
 
-            @st.cache_resource(show_spinner=False)
-            def load_data():
-                with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
-                    reader = SimpleDirectoryReader(input_dir="./kb", recursive=True)
-                    docs = reader.load_data()
-                    service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", 
-                                                                            temperature=0.5, 
-                                                                            system_prompt="You are a chat assistant for NXT. Keep your answers technical and based on facts – do not hallucinate features. . Don’t give information not mentioned in the CONTEXT INFORMATION."))
-                    index = VectorStoreIndex.from_documents(docs, service_context=service_context)
-                    return index
+        @st.cache_resource(show_spinner=False)
+        def load_data():
+            with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
+                reader = SimpleDirectoryReader(input_dir="./kb", recursive=True)
+                docs = reader.load_data()
+                service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", 
+                                                                        temperature=0.5, 
+                                                                        system_prompt="You are a chat assistant for NXT. Keep your answers technical and based on facts – do not hallucinate features. . Don’t give information not mentioned in the CONTEXT INFORMATION."))
+                index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+                return index
 
-            index = load_data()
+        index = load_data()
 
-            chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
-            if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
-                st.session_state.messages.append({"role": "user", "content": prompt})
+        chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+        if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+            st.session_state.messages.append({"role": "user", "content": prompt})
 
-            for message in st.session_state.messages: # Display the prior chat messages
-                with st.chat_message(message["role"]):
-                    st.write(message["content"])
-            
-            if st.session_state.messages[-1]["role"] != "assistant":
-                with st.chat_message("assistant"):
-                    with st.spinner("Thinking..."):
-                        response = chat_engine.chat(prompt)
-                        st.write(response.response)
-                        message = {"role": "assistant", "content": response.response }
-                        st.session_state.messages.append(message) # Add response to message history
-        except: 
-            st.write('Try Refreshing the page. This error is caused by OpenAI\'s limit for free trial')
+        for message in st.session_state.messages: # Display the prior chat messages
+            with st.chat_message(message["role"]):
+                st.write(message["content"])
+        
+        if st.session_state.messages[0]["role"] != "assistant":
+            with st.chat_message("assistant"):
+                with st.spinner("Thinking..."):
+                    response = chat_engine.chat(prompt)
+                    st.write(response.response)
+                    message = {"role": "assistant", "content": response.response }
+                    st.session_state.messages.append(message) # Add response to message history
     else: 
         st.header("Chatbot v1")
         st.subheader("Using system prompt as knowledge base")
